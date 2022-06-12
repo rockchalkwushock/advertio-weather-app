878092eaf678b6d002a0799d1145a775eaa3d91c
Cody Brunner <me@codybrunner.com>
2022-06-12 08:22:34 -0500

    chore(root): adds api key

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test'
+    REACT_APP_OWM_API_KEY: string
+  }
+}
